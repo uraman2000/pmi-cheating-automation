dc22cae13d89b7816e23649e5410d383ba2ee1f9
Pol Imbing <uraman2000@gmail.com>
2023-07-08 04:43:15 +0800

    mommy face

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,13 @@
+import image from "../assets/mommy.png"
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-7 w-auto border-t-2 border-b-2 border-gray-900">
+      {/* <div className="animate-spin rounded-full h-7 w-auto border-t-2 border-b-2 border-gray-900">
         mommy taba
+      </div> */}
+      <div className="animate-spin flex flex-col">
+        <img className=" h-auto w-14" src={image} alt="mommy taba" />
+        
       </div>
     </div>
   )
